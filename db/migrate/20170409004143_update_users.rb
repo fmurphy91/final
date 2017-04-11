6a908689ead52af1a9885c5841fa382f4e0e63c8
Real Name <fmurphynci2016@gmail.com>
2017-04-11 05:34:52 +0100

    your commit message

--- db/migrate/20170409004143_update_users.rb
+++ db/migrate/20170409004143_update_users.rb
@@ -2,6 +2,7 @@
 class UpdateUsers < ActiveRecord::Migration[5.0]
   def change
     @u = User.find_by( email: 'fintanmurphy10@gmail.com' )
+    @u = User.find_by( email: 'fmurphynci2017@gmail.com' )
     @u.update_attribute :admin, true
   end
 end
