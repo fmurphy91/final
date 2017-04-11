a6f7ec7f3cdea02510c7444c0125896a872b8fdc
Real Name <fmurphynci2016@gmail.com>
2017-04-11 15:48:44 +0100

    your commit message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery.raty
 //= require bootstrap
 //= require jquery.turbolinks
@@ -18,5 +17,6 @@
 //= require customjsfile
 //= require global
 //= require bootstrap/dropdown
+//= require jquery
 //= require turbolinks
 //= require_tree .
