860af61db4fc04813bbcba59a91feb284dc61198
Real Name <fmurphynci2016@gmail.com>
2017-04-11 04:44:23 +0100

    your commit message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,8 @@ Rails.application.configure do
 
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
+config.action_mailer.default_url_options = { host: 'stormy-chamber-20000.herokuapp.com' }
+
 
 ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
