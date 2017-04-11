4c2afee56ca1e558c00af2610addf5106100d668
Real Name <fmurphynci2016@gmail.com>
2017-04-11 04:34:43 +0100

    your commit message

--- db/migrate/20170409004143_update_users.rb
+++ db/migrate/20170409004143_update_users.rb
@@ -1,7 +1,3 @@
 # Migration used to assign admin user to enable admin email listed here to only destroy/delete paatient/appointments and doctor records
 class UpdateUsers < ActiveRecord::Migration[5.0]
-  def change
-    @u = User.find_by( email: 'admin@doctor.com' )
-    @u.update_attribute :admin, true
-  end
 end
